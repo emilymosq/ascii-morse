81f520af1e3aa3cbdeb33cc1cf6778a5077f55d3
emilymosq <emilymosq@gmail.com>
2024-11-14 11:05:53 +0100

    HasMap de ascii a morse

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, String> asciiMorse= new HashMap<>();
+        asciiMorse.put("A", ".-");
+        asciiMorse.put("B", "-...");
+        asciiMorse.put("C", "-.-.");
+        asciiMorse.put("D", "-..");
+        asciiMorse.put("E", ".");
+        asciiMorse.put("F", "..-.");
+        asciiMorse.put("G", "--.");
+        asciiMorse.put("H", "....");
+        asciiMorse.put("I", "..");
+        asciiMorse.put("J", ".---");
+        asciiMorse.put("K", "-.-");
+        asciiMorse.put("L", ".-..");
+        asciiMorse.put("M", "--");
+        asciiMorse.put("N", "-.");
+        asciiMorse.put("O", "---");
+        asciiMorse.put("P", ".--.");
+        asciiMorse.put("Q", "--.-");
+        asciiMorse.put("R", ".-.");
+        asciiMorse.put("S", "...");
+        asciiMorse.put("T", "-");
+        asciiMorse.put("U", "..-");
+        asciiMorse.put("V", "...-");
+        asciiMorse.put("W", ".--");
+        asciiMorse.put("X", "-..-");
+        asciiMorse.put("Y", "-.--");
+        asciiMorse.put("Z", "--..");
+        asciiMorse.put("Ñ", ".----.");
+
+
+    }
+}
